31625dbc04ebf6bbcdfdc299981011256f15125b
atmanad <atmanad0001@gmail.com>
2020-12-20 09:54:48 +0530

    final commit

--- frontend/src/components/DestinationCarousel.jsx
+++ frontend/src/components/DestinationCarousel.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Carousel from 'react-elastic-carousel'
 import { Link } from 'react-router-dom'
-import taj_mahal from '../assets/img/utter_pradesh/taj_mahal.jpg'
 import red_fort from '../assets/img/delhi/red_fort.jpg'
-import city_palace from '../assets/img/rajasthan/city_palace.jpg'
+import hawaMahal from '../assets/img/rajasthan/hawa-mahal1.jpg'
 import jagannath_puri from '../assets/img/odisha/jagannath_puri.png'
 
-//import  from '../Assets/images/taj_mahal.jpg'
 function DestinationCarousel() {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -20,52 +18,29 @@ function DestinationCarousel() {
           <img src={red_fort} className="card-img-top" alt="..." />
           <div className="card-body top-state-body">
             <h3>Red Fort, Delhi</h3>
-            <h5>Delhi</h5>
             <p className="card-text text-muted text-left">The Red Fort is a historical fortification in the old Delhi area. Shah Jahan constructed it in the year 1639 as a result of a capital shift from Agra to Delhi. Used as the main residence of the emperors of the Mughal dynasty, this imposing piece of architecture derives its name from its impregnable red sandstone walls.</p>
           </div>
         </div>
       </Link>
-      <Link className="px-3 nav-link state" to="/">
+      <Link className="px-3 nav-link state" to="/top-places/Puri">
         <div className="card" >
           <img src={jagannath_puri} className="card-img-top" alt="..." />
           <div className="card-body top-state-body">
-            <h3>Puri Jagannath Temple, Puri</h3>
-            <h5>Odisha</h5>
+            <h3>Puri Jagannath Temple, Odisha</h3>
             <p className="card-text text-muted text-left">Located in the sacred town of Puri, the Jagannath Temple or the pride of India . This glorious temple is the abode of Lord Jagannath who is a form of Lord Vishnu. It is the most revered pilgrimage site for Hindus and is included in the pious Char Dham Yatra with Badrinath, Dwarka and Rameshwaram.</p>
           </div>
         </div>
       </Link>
       <Link className="px-3 nav-link state" to="/">
         <div className="card" >
-          <img src={city_palace} className="card-img-top" alt="..." />
+          <img src={hawaMahal} className="card-img-top" alt="..." />
           <div className="card-body top-state-body">
-            <h3>City Palace, Udaipur</h3>
-            <h5>Rajasthan</h5>
-            <p className="card-text text-muted text-left">Built on the banks of Lake Pichola, the City Palace in Udaipur is considered to be the largest royal complex in Rajasthan. The magnificent palace was built in the year 1559 by Maharana Uday Singh and served as the main seat of power, where the Maharanas lived and administered the kingdom from.</p>
+            <h3>Hawa Mahal, Rajasthan</h3>
+            <p className="card-text text-muted text-left">
+              Hawa Mahal is a palace in Jaipur, India approximately 300 kilometers from the capital city of Delhi. Built from red and pink sandstone, the palace sits on the edge of the City Palace, Jaipur, and extends to the Zenana, or women's chambers. The structure was built in 1799 by Maharaja Sawai Pratap Singh, the grandson of Maharaja Sawai Jai Singh, who was the founder of Jaipur              </p>
           </div>
         </div>
       </Link>
-      <Link className="px-3 nav-link state" to="/" >
-        <div className="card" >
-          <img src={taj_mahal} className="card-img-top" alt="..." />
-          <div className="card-body top-state-body">
-            <h3>Taj Mahal, Agra</h3>
-            <h5>Uttar Pradesh</h5>
-            <p className="card-text text-muted text-left">One of the seven wonders of the world, Taj Mahal is located on the banks of River Yamuna in Agra. It was built by the Mughal Emperor Shah Jahan as a memorial for his third wife, Mumtaz Mahal. Constructed entirely out of white marble in the 17th century, Taj Mahal is one of the most beautiful monuments in the world.</p>
-          </div>
-        </div>
-      </Link>
-      {/* <Link className="px-3 nav-link state" to="/">
-        <div className="card" >
-          <img src={red_fort} className="card-img-top" alt="..." />
-          <div className="card-body top-state-body">
-            <h3>place, city</h3>
-            <h5>state</h5>
-            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </Link> */}
-
     </Carousel>
   )
 
